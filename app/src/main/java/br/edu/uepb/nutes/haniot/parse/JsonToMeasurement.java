13210cb04091a8d2fb9cd3420936e1174b50d3f9
Douglas Rafael <douglas.rafael@nutes.uepb.edu.br>
2018-01-12 02:29:20 -0300

    Implementação inicial do parse JSON para Measurement

--- app/src/main/java/br/edu/uepb/nutes/haniot/parse/JsonToMeasurement.java
+++ app/src/main/java/br/edu/uepb/nutes/haniot/parse/JsonToMeasurement.java
@@ -1,5 +1,6 @@
 package br.edu.uepb.nutes.haniot.parse;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import br.edu.uepb.nutes.haniot.model.Measurement;
@@ -14,9 +15,82 @@ import br.edu.uepb.nutes.haniot.model.MeasurementType;
  */
 public class JsonToMeasurement {
 
-    public static Measurement parse(JSONObject json, int measurementType) {
-        Measurement  measurement;
+    public static Measurement parse(JSONObject json, int measurementType) throws JSONException {
+        switch (measurementType) {
+            case MeasurementType.TEMPERATURE:
+                return jsonToMeasurementTemperature(json);
+            case MeasurementType.BLOOD_GLUCOSE:
+            default:
+                break;
+        }
 
         return null;
+    }
+
+    /**
+     * Convert json to Temperature Measurement.
+     *
+     * @param json
+     * @return Measurement
+     * @throws JSONException
+     */
+    public static Measurement jsonToMeasurementTemperature(JSONObject json) throws JSONException {
+        Measurement measurement = new Measurement(
+                json.getString("temperature"),
+                json.getString("temperatureUnit"),
+                json.getLong("timestamp"),
+                MeasurementType.TEMPERATURE);
+
+        return measurement;
+    }
+
+    /**
+     * Convert json to Body Mass Measurement.
+     *
+     * @param json
+     * @return Measurement
+     */
+    public Measurement jsonToMeasurementBodyMass(JSONObject json) throws JSONException {
+        Measurement measurement = new Measurement(
+                json.getString("bodyMass"),
+                json.getString("bodyMassUnit"),
+                json.getLong("timestamp"),
+                MeasurementType.BODY_MASS);
+
+        return measurement;
+    }
+
+    /**
+     * Convert json to Body Fat Measurement.
+     *
+     * @param json
+     * @return Measurement
+     * @throws JSONException
+     */
+    public Measurement jsonToMeasurementBodyFat(JSONObject json) throws JSONException {
+        Measurement measurement = measurement = new Measurement(
+                json.getString("bodyFat"),
+                json.getString("bodyFatUnit"),
+                json.getLong("timestamp"),
+                MeasurementType.BODY_FAT);
+
+        return measurement;
+    }
+
+    /**
+     * Convert json to HeartRate Measurement.
+     *
+     * @param json
+     * @return Measurement
+     * @throws JSONException
+     */
+    public Measurement jsonToMeasurementHeartRate(JSONObject json) throws JSONException {
+        Measurement measurement = new Measurement(
+                json.getString("heartRate"),
+                json.getString("heartRateUnit"),
+                json.getLong("timestamp"),
+                MeasurementType.HEART_RATE);
+
+        return measurement;
     }
 }
