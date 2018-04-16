0ea9aa44459f9d1ebecabb9ae06c97cdc5483c79
Douglas Rafael <douglas.rafael@nutes.uepb.edu.br>
2018-04-16 18:58:14 -0300

    Adicionado telas assessment 1-4

--- app/src/main/java/br/edu/uepb/nutes/haniot/elderly/assessment/FallRiskActivity.java
+++ app/src/main/java/br/edu/uepb/nutes/haniot/elderly/assessment/FallRiskActivity.java
@@ -3,6 +3,7 @@ package br.edu.uepb.nutes.haniot.elderly.assessment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.github.paolorotolo.appintro.AppIntro;
@@ -18,13 +19,39 @@ import br.edu.uepb.nutes.haniot.R;
  */
 public class FallRiskActivity extends AppIntro implements SliderPagerFragment.OnResponseListener {
     private final String TAG = "FallRiskActivity";
+    private Boolean responseQ1;
+    private Boolean responseQ2;
+    private Boolean responseQ3;
+    private Boolean responseQ4;
+    private Boolean responseQ5;
+    private Boolean responseQ6;
+    private Boolean responseQ7;
+    private Boolean responseQ8;
+    private Boolean responseQ9;
+    private Boolean responseQ10;
+    private int currentQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setFadeAnimation();
+        showSkipButton(true);
+        setColorTransitionsEnabled(true);
+        setSkipText("Cancelar");
 
-        addSlide(SliderPagerFragment.newInstance(R.layout.fragment_elderly_fall_risk_q1));
-        addSlide(SliderPagerFragment.newInstance(R.layout.fragment_elderly_fall_risk_q2));
+        setImmersiveMode(true);
+        addSlide(SliderPagerFragment.newInstance(this, R.layout.fragment_elderly_fall_risk_q1));
+        addSlide(SliderPagerFragment.newInstance(this, R.layout.fragment_elderly_fall_risk_q2));
+        addSlide(SliderPagerFragment.newInstance(this, R.layout.fragment_elderly_fall_risk_q3));
+        addSlide(SliderPagerFragment.newInstance(this, R.layout.fragment_elderly_fall_risk_q4));
+        addSlide(SliderPagerFragment.newInstance(this, R.layout.fragment_elderly_fall_risk_q4));
+        addSlide(SliderPagerFragment.newInstance(this, R.layout.fragment_elderly_fall_risk_q4));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        currentQuestion = 1;
     }
 
     @Override
@@ -42,19 +69,62 @@ public class FallRiskActivity extends AppIntro implements SliderPagerFragment.On
     @Override
     public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
         super.onSlideChanged(oldFragment, newFragment);
-        if (oldFragment != null)
-            Log.d(TAG, "onSlideChanged() - old: ");
+        if (oldFragment != null) {
+            SliderPagerFragment page = (SliderPagerFragment) oldFragment;
+            Log.d(TAG, "currentQuestion " + currentQuestion + " values: " + responseQ1 + " | " + responseQ2);
+        }
     }
 
     @Override
     public void onResponse(boolean response, int layoutResId) {
-        if (layoutResId == R.layout.fragment_elderly_fall_risk_q1)
-            Log.d(TAG, "onResponse() - page1: " + response);
-        else if (layoutResId == R.layout.fragment_elderly_fall_risk_q2)
-            Log.d(TAG, "onResponse() - page2: " + response);
-        else
-            Log.d(TAG, "onResponse() - No page");
-
-
+        switch (layoutResId) {
+            case R.layout.fragment_elderly_fall_risk_q1:
+                Log.d(TAG, "onResponse() - page1: " + response);
+                currentQuestion = 1;
+                responseQ1 = response;
+                break;
+            case R.layout.fragment_elderly_fall_risk_q2:
+                Log.d(TAG, "onResponse() - page2: " + response);
+                currentQuestion = 2;
+                responseQ2 = response;
+                break;
+            case R.layout.fragment_elderly_fall_risk_q3:
+                Log.d(TAG, "onResponse() - page3: " + response);
+                currentQuestion = 3;
+                responseQ3 = response;
+                break;
+            default:
+                break;
+        }
     }
+
+    @Override
+    public void nextBlockPage(boolean block) {
+//        setNextPageSwipeLock(block);
+    }
+
+    // TODO TESTAR!!!
+//    public void toggleNextPageSwipeLock(View v) {
+//        AppIntroViewPager pager = getPager();
+//        boolean pagingState = pager.isNextPagingEnabled();
+//        setNextPageSwipeLock(pagingState);
+//    }
+
+//    public void toggleSwipeLock(View v) {
+//        AppIntroViewPager pager = getPager();
+//        boolean pagingState = pager.isPagingEnabled();
+//        setSwipeLock(pagingState);
+//    }
+//
+//    public void toggleProgressButton(View v) {
+//        boolean progressButtonState = isProgressButtonEnabled();
+//        progressButtonState = !progressButtonState;
+//        setProgressButtonEnabled(progressButtonState);
+//    }
+//
+//    public void toggleSkipButton(View v) {
+//        boolean skipButtonState = isSkipButtonEnabled();
+//        skipButtonState = !skipButtonState;
+//        showSkipButton(skipButtonState);
+//    }
 }
--- app/src/main/java/br/edu/uepb/nutes/haniot/elderly/assessment/SliderPagerFragment.java
+++ app/src/main/java/br/edu/uepb/nutes/haniot/elderly/assessment/SliderPagerFragment.java
@@ -2,16 +2,22 @@ package br.edu.uepb.nutes.haniot.elderly.assessment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroBase;
+import com.github.paolorotolo.appintro.AppIntroViewPager;
+import com.github.paolorotolo.appintro.ISlideBackgroundColorHolder;
 
 import br.edu.uepb.nutes.haniot.R;
+import br.edu.uepb.nutes.haniot.utils.Log;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -22,20 +28,39 @@ import butterknife.ButterKnife;
  * @version 1.0
  * @copyright Copyright (c) 2017, NUTES UEPB
  */
-public class SliderPagerFragment extends Fragment {
+public class SliderPagerFragment extends Fragment implements ISlideBackgroundColorHolder {
+    private final String TAG = "SliderPagerFragment";
     private static final String ARG_LAYOUT_RES_ID = "arg_layout_id";
+    private static final String ARG_BACK_COLOR_RES_ID = "arg_back_color_id";
+    private static Context mContext;
 
     private int layoutResId;
+    private int backColorResId;
     private OnResponseListener mListener;
+    private View view;
+    private boolean isBlocked;
 
     @BindView(R.id.question_radioGroup)
     RadioGroup radioGroup;
 
+    @BindView(R.id.yes_radioButton)
+    RadioButton radioYes;
+
+    @BindView(R.id.no_radioButton)
+    RadioButton radioNo;
+
     public SliderPagerFragment() {
     }
 
-    public static SliderPagerFragment newInstance(int layoutResId) {
+    /**
+     * Get instance fragment.
+     *
+     * @param layoutResId
+     * @return SliderPagerFragment
+     */
+    public static SliderPagerFragment newInstance(Context context, int layoutResId) {
         SliderPagerFragment sampleSlide = new SliderPagerFragment();
+        mContext = context;
 
         Bundle args = new Bundle();
         args.putInt(ARG_LAYOUT_RES_ID, layoutResId);
@@ -44,12 +69,36 @@ public class SliderPagerFragment extends Fragment {
         return sampleSlide;
     }
 
+    /**
+     * Get instance fragment.
+     *
+     * @param layoutResId
+     * @param backColorResId
+     * @return SliderPagerFragment
+     */
+    public static SliderPagerFragment newInstance(Context context, int layoutResId, int backColorResId) {
+        SliderPagerFragment sampleSlide = new SliderPagerFragment();
+        mContext = context;
+
+        Bundle args = new Bundle();
+        args.putInt(ARG_LAYOUT_RES_ID, layoutResId);
+        args.putInt(ARG_BACK_COLOR_RES_ID, backColorResId);
+        sampleSlide.setArguments(args);
+
+        return sampleSlide;
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("TEST", "onCreate() isBlocked: " + isBlocked);
+        nextPageBlock(true);
 
-        if (getArguments() != null && getArguments().containsKey(ARG_LAYOUT_RES_ID)) {
-            layoutResId = getArguments().getInt(ARG_LAYOUT_RES_ID);
+        if (getArguments() != null) {
+            if (getArguments().containsKey(ARG_LAYOUT_RES_ID))
+                layoutResId = getArguments().getInt(ARG_LAYOUT_RES_ID);
+            if (getArguments().containsKey(ARG_BACK_COLOR_RES_ID))
+                backColorResId = getArguments().getInt(ARG_BACK_COLOR_RES_ID);
         }
     }
 
@@ -57,26 +106,44 @@ public class SliderPagerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(layoutResId, container, false);
+        view = inflater.inflate(layoutResId, container, false);
         ButterKnife.bind(this, view);
+        Log.d("TEST", "onCreateView()" + "isBlockeed:" + isBlocked);
+
+        if (radioGroup.getCheckedRadioButtonId() == -1) // no checked
+            nextPageBlock(true);
+        else
+            nextPageBlock(false);
+
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            Log.d(TAG, "setOnCheckedChangeListener()");
             switch (checkedId) {
                 case R.id.yes_radioButton:
+                    Log.d(TAG, "yes_radioButton()" + layoutResId);
                     mListener.onResponse(true, layoutResId);
                     break;
                 case R.id.no_radioButton:
+                    Log.d(TAG, "no_radioButton()" + layoutResId);
                     mListener.onResponse(false, layoutResId);
                     break;
                 default:
+                    Log.d(TAG, "default()" + layoutResId);
                     break;
             }
+            nextPageBlock(false);
+        });
+    }
+
+    private void nextPage() {
+        final AppIntroViewPager page = ((AppIntro) mContext).getPager();
+        new Handler().post(() -> {
+            page.goToNextSlide();
         });
     }
 
@@ -94,9 +161,39 @@ public class SliderPagerFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+        Log.d("TEST", "onDetach()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("TEST", "onDestroy()");
+    }
+
+    @Override
+    public int getDefaultBackgroundColor() {
+        return backColorResId;
+    }
+
+    @Override
+    public void setBackgroundColor(int backgroundColor) {
+        if (backColorResId != 0)
+            view.setBackgroundColor(backColorResId);
     }
 
     public interface OnResponseListener {
         void onResponse(boolean response, int layoutResId);
+
+        void nextBlockPage(boolean block);
+    }
+
+    public void nextPageBlock(boolean block) {
+        mListener.nextBlockPage(block);
+        isBlocked = block;
+        if (mContext instanceof AppIntro) {
+            AppIntro appIntro = (AppIntro) mContext;
+            appIntro.setNextPageSwipeLock(block);
+//            appIntro.setBackButtonVisibilityWithDone(block);
+        }
     }
 }
