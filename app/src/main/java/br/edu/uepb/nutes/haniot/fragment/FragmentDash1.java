2d6fbd24c153041bf27bb00f9864146681681aec
augustoboliveira <augusto.bezerra@nutes.uepb.edu.br>
2018-08-13 16:53:59 -0300

    .

--- app/src/main/java/br/edu/uepb/nutes/haniot/fragment/FragmentDash1.java
+++ app/src/main/java/br/edu/uepb/nutes/haniot/fragment/FragmentDash1.java
@@ -1,12 +1,10 @@
 package br.edu.uepb.nutes.haniot.fragment;
 
-import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
